44e746fb1d03e086233b7a0d206ec01ea7ab81c6
Murilo Cardoso <cardoso.murilo2002@gmail.com>
2024-11-06 14:10:14 -0300

    feat: updates to list all messages with an assistant

--- app/repository/ai_repository.py
+++ app/repository/ai_repository.py
@@ -9,31 +9,34 @@ class AiRepository(AiRepositoryPort):
     def __init__(self, ai_client: OpenAI, ai_assistant: Assistant):
         self.client = ai_client
         self.assistant = ai_assistant
+        self.thread = self.client.beta.threads.create()
 
     def answer_message(self, message: str):
 
         try:
-            thread = self.client.beta.threads.create()
 
             ai_message = self.client.beta.threads.messages.create(
-                thread_id=thread.id,
+                thread_id=self.thread.id,
                 role="user",
                 content=message
             )
 
             run = self.client.beta.threads.runs.create(
-                thread_id=thread.id,
+                thread_id=self.thread.id,
                 assistant_id=self.assistant.id
             )
 
             while run.status == 'queued' or run.status == 'in_progress':
                 run = self.client.beta.threads.runs.retrieve(
-                    thread_id=thread.id,
+                    thread_id=self.thread.id,
                     run_id=run.id,
                 )
                 time.sleep(0.3)
 
-            messages = self.client.beta.threads.messages.list(thread_id=thread.id, order="asc", after=ai_message.id)
+            messages = self.client.beta.threads.messages.list(
+                thread_id=self.thread.id,
+                order="desc",
+            )
 
             return messages.model_dump_json()
         except Exception as e:
